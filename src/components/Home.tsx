f9adccbfd1d4f2efe830ff5bf421fb6f6f05b1f7
Carlos Tapia <Tapiacarlos32@gmail.com>
2025-03-25 19:02:26 -0500

    Add Home Component

---
+++ src/components/Home.tsx
@@ -0,0 +1,24 @@
+import Container from "react-bootstrap/Container";
+import Gallery from './Gallery';
+const items = [
+  { id: 1 },
+  { id: 2 },
+  { id: 3 },
+  { id: 4 },
+  { id: 5 },
+  { id: 6 },
+  { id: 7 },
+  { id: 8 },
+  { id: 9 }
+]
+
+const Home = () => {
+  return (
+    <Container >
+      <h1 className="mb-4">Gallery</h1>
+      <Gallery items={items} />
+    </Container>
+  );
+}
+export default Home;
+
