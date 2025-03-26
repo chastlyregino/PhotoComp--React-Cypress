0f100c87e9f5cb9f008a571b3ae923d076c310f9
Carlos Tapia <Tapiacarlos32@gmail.com>
2025-03-26 14:13:24 -0500

    Add jest, and cypress with base test

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import Home from "./components/Home.tsx";
+import Home from './components/Home.tsx';
 
 function App() {
-  return (
-    <>
-      <h1>PhotoComp</h1>
-      <Home />
-    </>
-  );
+    return (
+        <>
+            <h1>PhotoComp</h1>
+            <Home />
+        </>
+    );
 }
 
 export default App;
-
