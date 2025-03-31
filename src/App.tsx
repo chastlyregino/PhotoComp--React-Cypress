e4de96116295d8c73da8991e714506b5dc0f539a
Carlos Tapia <Tapiacarlos32@gmail.com>
2025-03-31 15:07:56 -0500

    Add browserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import Home from './components/Home.tsx';
+import { Routes, Route } from "react-router-dom";
+
+import Home from './pages/Home';
 
 function App() {
-    return (
-        <>
-            <h1>PhotoComp</h1>
-            <Home />
-        </>
-    );
+  return (
+    <>
+      <h1>PhotoComp</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from "react-router";
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
