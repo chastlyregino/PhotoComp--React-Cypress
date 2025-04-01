5dde3fd221a6c945449cdc148c0fbe16e471a20b
Carlos Tapia <Tapiacarlos32@gmail.com>
2025-03-31 23:13:49 -0500

    Add auth context, and provider

---
+++ src/components/AuthContext.tsx
@@ -0,0 +1,21 @@
+import { createContext } from "react";
+
+export interface User {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  role: string;
+}
+
+export interface AuthContextType {
+  user: User | null;
+  token: string | null;
+  setUser: (user: User | null) => void;
+  setToken: (token: string | null) => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export default AuthContext;
--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,14 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router";
 import App from './App.tsx';
 
+import AuthProvider from "./components/AuthProvider.tsx"
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>
 );
