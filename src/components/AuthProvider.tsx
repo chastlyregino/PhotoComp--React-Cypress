f7ef8147130d549e0a1bf2d3f41030c978b82ee9
Carlos Tapia <Tapiacarlos32@gmail.com>
2025-03-31 23:13:49 -0500

    Add auth context, and provider

---
+++ src/components/AuthProvider.tsx
@@ -0,0 +1,34 @@
+import { useContext, useState, useEffect, ReactNode } from "react";
+import AuthContext, { User } from "./AuthContext";
+
+const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const context = useContext(AuthContext);
+
+  const [user, setUser] = useState<User | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    const storedToken = localStorage.getItem("token");
+
+    if (storedUser && storedToken) {
+      context?.setUser(JSON.parse(storedUser));
+      context?.setToken(storedToken);
+    }
+  }, [context]);
+
+  const logout = () => {
+    context?.setUser(null);
+    context?.setToken(null);
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, token, setUser, setToken, logout }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthProvider;
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,60 +1,114 @@
-import { useState } from "react";
+import AuthContext from "../components/AuthContext";
+import { useState, useContext } from "react";
 import { Form, Button, Container, Row, Col, Alert } from "react-bootstrap";
+import axiosInstance from "../components/axios"
 
 
 const Register: React.FC<{}> = () => {
+  const context = useContext(AuthContext);
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
   const [error, setError] = useState<string | null>(null);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (!email || !password) {
+    if (!email || !password || !username || !firstName || !lastName) {
       setError("Please fill in all fields");
       return;
     }
     setError(null);
-    // Meeting playground: Fetch
-    console.log("Logging in: ", { email, password });
-    return;
+    const body = { email, password, username, firstName, lastName };
+    try {
+
+      const response = await axiosInstance.post("/api/auth/register", body);
+      const token = response.data.token;
+      const user = response.data.user;
+
+      localStorage.setItem("token", token);
+      localStorage.setItem("user", JSON.stringify(user));
+
+      context?.setToken(token);
+      context?.setUser(user);
+
+    } catch (error) {
+      console.error(error);
+      setError("Registration Failed.")
+    }
   }
 
-  return (<>
-    <Container>
-      <Row className="justify-content-md-center">
-        <Col md={6}>
-          <h2 className="text-center">Register</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
+  return (
+    <>
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col md={6}>
+            <h2 className="text-center">Register</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form onSubmit={handleSubmit}>
 
-            <Form.Group controlId="formEmail" className="mb-3">
-              <Form.Label>Email Address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Form.Group>
+              <Form.Group controlId="formEmail" className="mb-3">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
 
-            <Form.Group controlId="formPassword" className="mb-3">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Enter password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </Form.Group>
+              <Form.Group controlId="formPassword" className="mb-3">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
 
-            <Button variant="primary" type="submit" className="w-100">
-              Login
-            </Button>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
-  </>);
+              <Form.Group controlId="formUsername" className="mb-3">
+                <Form.Label>User Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter a Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formFirstName" className="mb-3">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter First Name"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formLastName" className="mb-3">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Last Name"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </Form.Group>
+
+
+              <Button variant="primary" type="submit" className="w-100">
+                Login
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </>);
 
 }
 
