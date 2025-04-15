7b6aa23f26c145d4f06449f7973f8a2f2b26e18a
Bogdan Shmat <bogdan.shmat@wsu.edu>
2025-04-15 05:40:59 -0700

    Fixed component file

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  // Remove component testing configuration
   e2e: {
     baseUrl: 'http://localhost:5173',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
