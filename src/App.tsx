6f034cbfe30989155de78895f171459ae8603da7
Chastly Regino <chastly_regino@icloud.com>
2025-04-14 22:07:00 -0500

    Topbar fix

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/organizations" element={<Organizations />} />
             </Routes>
-            {/* <Footer /> */}
+            <Footer />
         </>
     );
 }
