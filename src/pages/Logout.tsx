36335050bc4ae843015c4351076ee45cf3f61bad
Carlos Tapia <Tapiacarlos32@gmail.com>
2025-04-16 11:55:05 -0500

    add logout

---
+++ src/pages/Logout.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import AuthContext from '../context/AuthContext';
+
+const Logout: React.FC = () => {
+  const { logout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    logout();
+    
+    navigate('/login');
+  }, [logout, navigate]);
+
+  return null;
+};
+
+export default Logout;
