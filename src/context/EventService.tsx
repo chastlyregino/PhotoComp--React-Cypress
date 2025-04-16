9d722e28da1bbf5fddccb10e25a7a7b988667373
Chastly Regino <chastly_regino@icloud.com>
2025-04-16 18:13:46 -0500

    Member can attend event

---
+++ src/context/EventService.tsx
@@ -0,0 +1,50 @@
+import axiosInstance, { noAuthInstance } from '../utils/axios';
+
+export interface EventUser {
+    PK: string; // USER#<ID>
+    SK: string; // EVENT#<ID>
+
+    id: string;
+
+    // GSI attributes
+    GSI2PK: string; // EVENT#<ID>
+    GSI2SK: string; // USER#<ID>
+}
+
+export interface EventUserResponse {
+    status: string;
+    data: {
+        userEvent: EventUser;
+    };
+}
+
+export interface attendeesResponse {
+    status: string;
+    data: {
+        userEvent: EventUser[];
+    };
+}
+
+export const attendEvent = async (orgId: string, eventId: string) => {
+    try {
+        const response = await axiosInstance.post<EventUserResponse>(
+            `/organizations/${orgId}/events/${eventId}`
+        );
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching attending event:', error);
+        throw error;
+    }
+};
+
+export const getEventAttendees = async (orgId: string, eventId: string) => {
+    try {
+        const response = await axiosInstance.get<attendeesResponse>(
+            `/organizations/${orgId}/events/${eventId}`
+        );
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching event attendees:', error);
+        throw error;
+    }
+};
