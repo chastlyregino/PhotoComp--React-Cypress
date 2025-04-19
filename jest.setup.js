7af7108b0feccb22b12b85dfb1a5afd2a711d11f
Carlos Tapia <Tapiacarlos32@gmail.com>
2025-04-18 20:25:54 -0500

    Fix the jest issues

--- jest.setup.js
+++
@@ -1,7 +0,0 @@
-import '@testing-library/jest-dom';
-import { TextEncoder, TextDecoder } from 'util';
-// Add TextEncoder polyfill
-if (typeof globalThis.TextEncoder === 'undefined') {
-    globalThis.TextEncoder = TextEncoder;
-    globalThis.TextDecoder = TextDecoder;
-}
