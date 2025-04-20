51af50947aca037af071f59cc13738683b928d59
Chastly Regino <chastly_regino@icloud.com>
2025-04-20 15:35:32 -0500

    baseURL

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const AXIOS_DEFAULTS = {
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://54.242.98.94:3000',
     headers: {
         'Content-Type': 'application/json',
     },
